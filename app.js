c9b69aedb67181fa6bae6e6348a958a1a7078744
Ander223 <ander_a_@hotmail.com>
2025-11-09 02:18:36 -0300

    primeiro commit - sorteio de números e limpa campos

---
+++ app.js
@@ -0,0 +1,37 @@
+let numeroAleatorio;
+                               // let numeroInseridoNaLista;
+
+
+function sortear(){
+    let quantidade = parseInt( document.getElementById('quantidade').value);   // captura os valores do elemento quantidade
+    let de = parseInt( document.getElementById('de').value);
+    let ate = parseInt( document.getElementById('ate').value);
+
+    let listaNumerosSorteados = [];
+   
+    for(let i = 1; i <= quantidade; i ++){             // controla a qtd de numeros aletorios que irão ser gerados
+
+        numeroAleatorio = geraNumerosAleatorios(de, ate);  // chama a função e envia 2 numeros por parametro e depois recebe 2 numeros por parametros para serem comparados no if
+
+        if(listaNumerosSorteados.includes(numeroAleatorio)){  // verifica se o numeroAleatorio esta na lista
+            numeroAleatorio = geraNumerosAleatorios(de, ate);                  // caso seja repetido chama a função novamente
+        } else {
+                listaNumerosSorteados.push(numeroAleatorio);  // se o numeroAleatorio não estiver na lista insere o mesmo
+            }  alert(`lista de numeros aleatorios ${listaNumerosSorteados}`); 
+      
+        
+    } limpaCampo();
+}
+   
+
+function geraNumerosAleatorios(min , max){
+    
+    return numeroAleatorio =  Math.floor(Math.random() * (max - min + 1)) + min;  // gera numerosAleatorios decimais
+}  
+
+function limpaCampo(){
+
+    quantidade =  document.getElementById('quantidade').value='';
+    de = document.getElementById('de').value='';
+    ate = document.getElementById('ate').value='';
+}
