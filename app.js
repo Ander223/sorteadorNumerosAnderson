c1f9a37353c093b03f9d4a9ba546976ea4c68e34
Ander223 <ander_a_@hotmail.com>
2025-11-09 16:07:18 -0300

    corrigindo numeros repetidos no if

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 let numeroAleatorio;
 
-
 function sortear(){
     let quantidade = parseInt( document.getElementById('quantidade').value);   // captura os valores do elemento quantidade
     let de = parseInt( document.getElementById('de').value);
@@ -10,17 +9,20 @@ function sortear(){
    
     for(let i = 1; i <= quantidade; i ++){             // controla a qtd de numeros aletorios que irão ser gerados
 
-        numeroAleatorio = geraNumerosAleatorios(de, ate);  // chama a função e envia 2 numeros por parametro e depois recebe 2 numeros por parametros para serem comparados no if
-
-        if(listaNumerosSorteados.includes(numeroAleatorio)){  // verifica se o numeroAleatorio esta na lista
-            numeroAleatorio = geraNumerosAleatorios(de, ate);                  // caso seja repetido chama a função novamente
+          numeroAleatorio = geraNumerosAleatorios(de, ate);  // chama a função e envia 2 numeros por parametro e depois recebe 2 numeros por parametros para serem comparados no if
+                                                                
+        if(listaNumerosSorteados.includes(numeroAleatorio)){  // verifica se o numeroAleatorio esta no array listaNumerosSorteados
+            let  numeroRepetidos = geraNumerosAleatorios(de, ate);                  // caso seja repetido chama a função novamente
+                                                                    console.log(`numeros sorteados repetidos ${numeroRepetidos}`);
+            i--;  // decrescenta 1 no i para gerar mais um número (novo número)
+          
         } else {
                 listaNumerosSorteados.push(numeroAleatorio);  // se o numeroAleatorio não estiver na lista insere o mesmo
-            } // alert(`lista de numeros aleatorios ${listaNumerosSorteados}`); 
+            } // console.log(`lista de numeros aleatorios ${listaNumerosSorteados}`); 
            
     } 
-    let numerosEscolhidos = document.getElementById('resultado').innerHTML=`<label class="texto__paragrafo">Números sorteados: ${listaNumerosSorteados}</label>`;  
-
+     document.getElementById('resultado').innerHTML=`<label class="texto__paragrafo">Números sorteados: ${listaNumerosSorteados}</label>`;  
+        // exibe os numeros sorteados na tela 
 } reiniciar();
 
    
@@ -35,16 +37,16 @@ function limpaCampo(){
     quantidade =  document.getElementById('quantidade').value=''; // inseri campo vazio no value
     de = document.getElementById('de').value='';
     ate = document.getElementById('ate').value='';
-    document.getElementById('resultado').innerHTML='<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
-   
+    document.getElementById('resultado').innerHTML='<label class="texto__paragrafo">Números sorteados:  Nenhum até agora</label>';
+                                                                    // volta p label com a mensagem inicial
 }
 
 function reiniciar(){
     let botaoReiniciar = document.getElementById('btn-reiniciar');
 
-    if(botaoReiniciar.classList.contains('container__botao-desabilitado')){
-        botaoReiniciar.classList.remove('container__botao-desabilitado');
-        botaoReiniciar.classList.add('container__botao');
+    if(botaoReiniciar.classList.contains('container__botao-desabilitado')){  // verifica se o botaoReiniciar contem a propriedade container__botao-desabilitado 
+        botaoReiniciar.classList.remove('container__botao-desabilitado');  // remove container__botao-desabilitado
+        botaoReiniciar.classList.add('container__botao');                // adiciona container__botao
     } else {
         botaoReiniciar.classList.add('container__botao');
         botaoReiniciar.classList.remove('container__botao-desabilitado');
